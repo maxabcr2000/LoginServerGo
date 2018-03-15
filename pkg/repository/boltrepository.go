a80f3512255199be20d97a69ca9d802f652a6416
Max <maxabcr2000@gmail.com>
2018-03-15 16:16:51 +0800

    rewrite boltrepository.go with more domain logic

--- pkg/repository/boltrepository.go
+++ pkg/repository/boltrepository.go
@@ -3,15 +3,30 @@ package repository
 import (
 	"github.com/boltdb/bolt"
 	"fmt"
+	"errors"
+	"encoding/json"
+	domain "github.com/maxabcr2000/LoginServerGo/pkg/domain"
 )
 
 type boltRepository struct{
 	db *bolt.DB
 }
 
+const (
+	BUCKET_NAME_USERS = "Users"
+)
+
 func CreateBoltRepository(dbName string) (*boltRepository,error) {
 	db, err := bolt.Open(dbName, 0600, nil)
 	if err != nil {
+		return nil, err
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(BUCKET_NAME_USERS))
+		return err
+	})
+	if err!=nil{
 		return nil, err
 	}
 
@@ -22,40 +37,39 @@ func CreateBoltRepository(dbName string) (*boltRepository,error) {
 	return repo, nil
 }
 
-func (repo *boltRepository) CreateBucket(bucketName string) error{
+func (repo *boltRepository) SaveUser(user *domain.User, key string) error{
 	return repo.db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte(bucketName))
-		return err
-	})
-}
-
-func (repo *boltRepository) SaveMessage(bucketName, key, value string) error{
-	err:=repo.db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucketName))
-		err := b.Put([]byte(key), []byte(value))
-		return err
-	})
-	if err!=nil{
-		return err
-	}
-	return nil
-}
-
-func (repo *boltRepository) ReadMessage(bucketName, key string) (string, error){
-	var resultBytes []byte
-	err:=repo.db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucketName))
+		b := tx.Bucket([]byte(BUCKET_NAME_USERS))
 		v := b.Get([]byte(key))
-		resultBytes = make([]byte, len(v))
-		copy(resultBytes, v)
-		fmt.Printf("key: %s , value: %s\n", key, resultBytes)
+		if v!=nil{
+			return errors.New("User account is already used")
+		}
+
+		body,_:= json.Marshal(user)
+		err := b.Put([]byte(key), body)
+		return err
+	})
+}
+
+func (repo *boltRepository) ReadUser(key string) (*domain.User, error){
+	user:= &domain.User{}
+	
+	err:=repo.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(BUCKET_NAME_USERS))
+		v := b.Get([]byte(key))
+		err := json.Unmarshal(v, user)
+		if err!=nil{
+			return err
+		}
+
+		fmt.Printf("key: %s , value: %s\n", key, user)
 		return nil
 	})
 	if err!=nil{
-		return "", err
+		return nil, err
 	}
 
-	return string(resultBytes), nil
+	return user, nil
 }
 
 func (repo *boltRepository) Close(){
